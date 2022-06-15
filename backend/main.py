159e0953291d5e1e8b372f967a7a9d1b8988fae3
Steven <stevendruesne@gmail.com>
2022-06-15 13:47:06 +0200

    Managed to send image to api

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,57 @@
 import uvicorn
-from fastapi import FastAPI, File, Uploadfile
+from fastapi import FastAPI, File, UploadFile
+
+import os
+import pathlib
 
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
+from PIL import Image
+from io import BytesIO
 
 app = FastAPI()
+
+model = load_model("modele_alpha_mich")
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to Scan'n'Sort API "}
 
-@app.get("/prediction")
-def predict():
-    model = load_model("modele_alpha_mich")
-    
+
+
+@app.post("/upload_file")
+async def read_root(file: UploadFile = File()):
+    # image = read_imagefile(await file.read())
+
+    image = file.file._file
+
+    # TO SAVE FILE
+    # file_location = f"{file.filename}"
+    # with open(file_location, "wb+") as file_object:
+    #     file_object.write(file.file.read())
+
+    #PREPROCESSING AND PREDICTION
     classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+    recyclable_lille = ["plastic","glass","metal","paper"]
 
-    path = "img_test.jpg"
-
-    img = tf.keras.preprocessing.image.load_img(path, target_size=(256, 256))
+    img = tf.keras.preprocessing.image.load_img(image, target_size=(256, 256))
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0) 
 
     predictions = model.predict(img_array)
 
-    return {"label":classes[np.argmax(predictions)] + f" {np.round(predictions[0][np.argmax(predictions)]*100, 2)}%",
-            "bin":"TEST_BIN"}
+    if classes[np.argmax(predictions)] in recyclable_lille:
+        bin = "recyclable"
+    else :
+        bin = "not recyclable"
 
-@app.get("/{test}")
-def test(test):
-    test = test + " plop"
-    return f"Hello {test}"
+
+    return {"info": f"file '{file.filename}'",
+            # "content": image,
+            "label":classes[np.argmax(predictions)] + f" {np.round(predictions[0][np.argmax(predictions)]*100, 2)}%",
+            "bin":bin}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
--- frontend/app.py
+++ frontend/app.py
@@ -1,35 +1,107 @@
 import streamlit as st
+import tensorflow as tf
 from tensorflow import keras
+from PIL import Image
+import io
+import os
+import numpy as np
 import requests
 
 
-# model = keras.models.load_model(r'C:\Users\sofia\Downloads\modele_nounou')
 
 URL = "http://host.docker.internal:8000"
 
-def load_image():
-    uploaded_file = st.file_uploader(label='Pick an image to test')
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        
-    st.button('Predict')
+classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+
+recyclable_lille = ["plastic","glass","metal","paper"]
+
+def load_model():
+    model = keras.models.load_model('modele_alpha_mich')
+    return model
+
     
-    # if st.button:
-    #     prediction = model.predict(uploaded_file)
-    #     st.write(prediction)
+
+# fonction test
+def load_image(img):
+    img = Image.open(img)
+    return img
+
+
+def get_image():
+    # get image
+    uploaded_file = st.file_uploader(label='Pick an image to test')
+
+    if uploaded_file:
+        file_details = {"Filename":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+        st.write(file_details)
+        img = load_image(uploaded_file)
+        st.image(img)
+
+        # save image
+        with open(uploaded_file.name,"wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+    return uploaded_file
+
+def predict(uploaded_file): #(model, image):
+    # load image
+    file = {'file': (uploaded_file.name, open(uploaded_file.name, 'rb'))}
+
+    # request API to make prediction 
+    response = requests.post(
+        f"{URL}/upload_file",
+        files=file,
+    )
+
+    #remove image
+    os.remove(f"{uploaded_file.name}")
+
+    return response.json()
 
     
 def main():
     st.title('Image upload demo')
-    x = requests.get('http://host.docker.internal:8000/')
-    st.write(x.json()["message"])
-    load_image()
+
+    uploaded_file = get_image()
+
+    result = st.button('Run on image')
+    if result:
+        pred = predict(uploaded_file)
+        st.write(pred)
+    
+#==============================================
+
+    # # get image
+    # uploaded_file = st.file_uploader(label='Pick an image to test')
+    # file_details = {"Filename":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+    # st.write(file_details)
+    # img = load_image(uploaded_file)
+    # st.image(img)
+
+    # # save image
+    # with open(uploaded_file.name,"wb") as f:
+    #     f.write(uploaded_file.getbuffer())
+
+    # # load image
+    # file = {'file': (uploaded_file.name, open(uploaded_file.name, 'rb'))}
+
+    # # request API to make prediction 
+    # response = requests.post(
+    #     f"{URL}/upload_file",
+    #     files=file,
+    # )
+
+    # #remove image
+    # os.remove(f"{uploaded_file.name}")
+
+    # st.write(response.json())
+
+#================================================
+#================================================
+
+    
+
 
 
 if __name__ == '__main__':
     main()
-    
-    
-
-
